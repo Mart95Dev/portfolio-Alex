b0323fdceecaf8193f13bf4814cba5c266103d45
Mart95Dev <d.wehrling@gmail.com>
2024-11-12 21:41:17 +0100

    test

---
+++ src/data/portfolioData.js
@@ -0,0 +1,45 @@
+export const portfolioData=[
+    {
+        id:1,
+        name:"OhMyFood",
+        languagesIcons:['fab fa-html5','fab fa-sass'],
+        source: 'https://github.com/bipba/Ohmyfood',
+        picture:'./media/ohMyFood.jpg',
+       },
+    {
+        id:2,
+        name:"Nina",
+        languagesIcons:['fab fa-html5','fab fa-js','fab fa-css3'],
+        source: 'https://github.com/bipba/nina',
+        picture:'./media/nino.jpg',
+      },
+    {
+        id:3,
+        name:"Architecte",
+        languagesIcons:['fab fa-js', 'fab fa-html5','fab fa-css3'],
+        source: 'https://github.com/bipba/FRONTENDFOLIO',
+        picture:'./media/sophie-bluel_11zon.jpg',
+   },
+    {
+        id:4,
+        name:"Kasa.fr",
+        languages:['react','sass'],
+        languagesIcons:['fab fa-react','fab fa-sass'],
+        source: 'https://github.com/bipba/kasa',
+        picture:'./media/kasa1.jpg',
+      },
+    {
+        id:5,
+        name:"ArgentBank.com",
+        languagesIcons:['fab fa-js','fab fa-react','fas fa-sync-alt','fab fa-sass'],
+        source: 'https://github.com/bipba/argent-bank-back-front',
+        picture:'./media/bank-tree.webp',
+    },
+    {
+        id:6,
+        name:"724Events.com",
+        languagesIcons:['fab fa-js','fab fa-react','fab fa-sass'],
+        source: 'https://github.com/bipba/Ohmyfood',
+        picture:'./media/724events300.png',
+   },
+]
